01c64635fde4ccc4d1fdf6d966dfd032abcd027c
Epic312 <kaden_ostendorf@yahoo.com>
2020-08-02 20:43:19 -0600

    azalea bush can be planted on compact azalea

--- src/main/java/com/teamaurora/hanami/common/block/AzaleaBushBlock.java
+++ src/main/java/com/teamaurora/hanami/common/block/AzaleaBushBlock.java
@@ -1,8 +1,12 @@
 package com.teamaurora.hanami.common.block;
 
 import com.teamabnormals.abnormals_core.common.blocks.AbnormalsFlowerBlock;
+import com.teamaurora.hanami.core.registry.HanamiBlocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
 import java.util.function.Supplier;
 
@@ -12,6 +16,11 @@ public class AzaleaBushBlock extends AbnormalsFlowerBlock {
     public AzaleaBushBlock(Supplier<Effect> effect, int stewEffectDuration, Properties properties) {
         super(Effects.BLINDNESS, stewEffectDuration, properties);
         this.stewEffect = effect;
+    }
+
+    @Override
+    protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return state.getBlock() == HanamiBlocks.COMPACT_AZALEA.get() || super.isValidGround(state, worldIn, pos);
     }
 
     public Effect getStewEffect() {
