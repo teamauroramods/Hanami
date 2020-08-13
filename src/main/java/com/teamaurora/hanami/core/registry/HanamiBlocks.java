a4db12fd98e77225dd100358e328d3a379d01872
Epic312 <kaden_ostendorf@yahoo.com>
2020-08-13 15:42:48 -0600

    started work on fruitful compat

--- src/main/java/com/teamaurora/hanami/core/registry/HanamiBlocks.java
+++ src/main/java/com/teamaurora/hanami/core/registry/HanamiBlocks.java
@@ -55,6 +55,8 @@ public class HanamiBlocks {
     public static final RegistryObject<Block> SAKURA_LADDER = HELPER.createCompatBlock("quark","sakura_ladder", ()->new AbnormalsLadderBlock(Block.Properties.from(Blocks.LADDER).harvestTool(ToolType.AXE)), ItemGroup.DECORATIONS);
     public static final Pair<RegistryObject<AbnormalsChestBlock>, RegistryObject<AbnormalsTrappedChestBlock>> SAKURA_CHESTS = HELPER.createCompatChestBlocks("sakura", MaterialColor.ADOBE);
 
+    public static final RegistryObject<Block> CHERRY_SAKURA_LEAVES = HELPER.createCompatBlock("fruitful", "cherry_sakura_leaves", ()->new AbnormalsLeavesBlock(Block.Properties.from(SAKURA_LEAVES.get()).notSolid().harvestTool(ToolType.HOE)), ItemGroup.DECORATIONS);
+
     public static final RegistryObject<Block> SAKURA_BEEHIVE = HELPER.createCompatBlock("buzzier_bees", "sakura_beehive", ()->new AbnormalsBeehiveBlock(Block.Properties.from(Blocks.BEEHIVE)), ItemGroup.DECORATIONS);
 
     // plants
